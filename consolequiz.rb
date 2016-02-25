96c4915629680fffbd6d345586e80f7b21f87a08
enemali <malitician@gmail.com>
2016-02-25 14:26:58 +0100

    Restriction on the use of certain keys was enforced

--- consolequiz.rb
+++ consolequiz.rb
@@ -1,17 +1,46 @@
 require 'json'
-
+class Quizapp
+  myscore = 0
 file = File.read('Physics.json')
 question = JSON.parse(file)
 subject = {}
 basedir = '.'
-alljson = Dir.glob("*.json")
-puts alljson
-puts "Use the Numerical Keys to navigate around the questions"
+#alljson = Dir.glob("*.json")
+#puts alljson
 
- start = false
- while !start
-   qsn = gets.to_i-1
-   if qsn.between?(0,5)
+
+  puts "Use the Numerical Keys to navigate around the questions"
+  puts
+  puts "+--+--------------------------+--+
+|  | CONSOLE QUIZ APPLICATION |  |
++--+--------------------------+--+
++  +      Made by Enemali     +  +
+|  |                          |  |
++  +         at               +  +
+|  |                          |  |
++  +  Andela Boot Camp        +  +
+|  |        2016              |  |
++--+--------------------------+--+
+|  |         Andela           |  |
++--+--------------------------+--+"
+  puts " Enter 1 to start the quiz and your time will beging "
+  askuser = false
+  while !askuser do
+    request = gets.to_i
+    if request == 1
+      askuser = true
+    end
+
+  end
+  system ("cls")
+
+
+
+start = false
+  puts " Numerical keys to switch to another question"
+while !start
+  qsn = gets.to_i-1
+  if qsn.between?(0,5)
     subject = question.values[qsn]
     system ("cls")
     puts subject.keys[0]
@@ -22,10 +51,21 @@ puts "Use the Numerical Keys to navigate around the questions"
     puts subject.keys[2]+" "+subject.values[2]
     puts subject.keys[3]+" "+subject.values[3]
     puts subject.keys[4]+" "+subject.values[4]
-   else
-     puts "Enter Numbers between 0 to 5"
-     system ("cls")
-     puts "Enter Numbers between 0 to 5"
-   end
+    puts
+    puts "Use the keyboard to select A  ,  B  , C  or  D"
+    myans = gets.chomp
+    if "D" == myans.upcase
+      myscore +=1
+      puts myscore
+    else
+      puts "no"
+    end
+  else
+    puts "Enter Numbers between 0 to 5"
+    system ("cls")
+    puts "Enter Numbers between 0 to 5"
+  end
+
+end
 end
 
