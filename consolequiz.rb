611d5f335e513fc37d8c4b2cdcc440732773550c
enemali <malitician@gmail.com>
2016-02-25 07:35:06 +0100

    CLI functionanility added

--- consolequiz.rb
+++ consolequiz.rb
@@ -1,35 +1,31 @@
- require 'json'
-file = File.read('qsn.json')
+require 'json'
+
+file = File.read('Physics.json')
 question = JSON.parse(file)
 subject = {}
-subject = question.values[1]
-#puts question.keys[0]
-puts subject.keys[0]
-puts subject.values[0]
-puts subject.keys[1]+" "+subject.values[1]
-puts subject.keys[2]+" "+subject.values[2]
-puts subject.keys[3]+" "+subject.values[3]
-puts subject.keys[4]+" "+subject.values[4]
+basedir = '.'
+alljson = Dir.glob("*.json")
+puts alljson
+puts "Use the Numerical Keys to navigate around the questions"
 
+ start = false
+ while !start
+   qsn = gets.to_i-1
+   if qsn.between?(0,5)
+    subject = question.values[qsn]
+    system ("cls")
+    puts subject.keys[0]
+    puts
+    puts subject.values[0]
+    puts
+    puts subject.keys[1]+" "+subject.values[1]
+    puts subject.keys[2]+" "+subject.values[2]
+    puts subject.keys[3]+" "+subject.values[3]
+    puts subject.keys[4]+" "+subject.values[4]
+   else
+     puts "Enter Numbers between 0 to 5"
+     system ("cls")
+     puts "Enter Numbers between 0 to 5"
+   end
+end
 
-
-
-#puts data_hash[data_hash.keys[0]]
-
-
-
-#puts data_hash['posts']
-
-
-
-#puts data_hash.keys[1]+" - "+data_hash[data_hash.keys[1]]
-#puts data_hash.keys[2]+" - "+data_hash[data_hash.keys[2]]
-#puts data_hash.keys[3]+" - "+data_hash[data_hash.keys[3]]
-#puts data_hash.keys[4]+" - "+data_hash[data_hash.keys[4]]
-#puts data_hash.keys[5]+" - "+data_hash[data_hash.keys[5]]
-
-
-
-#puts data_hash['posts'].keys
-#puts data_hash['posts'].first[0]
-#puts data_hash['posts'].first[1]
