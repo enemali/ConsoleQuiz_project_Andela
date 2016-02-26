af5988d977c7c6aa3e41ed7f4cb05ca1fbcfffe3
enemali <malitician@gmail.com>
2016-02-26 04:22:40 +0100

    Automation of the CLI was improved

--- consolequiz.rb
+++ consolequiz.rb
@@ -1,74 +1,72 @@
 require 'json'
-class Quizapp
   myscore = 0
-file = File.read('Physics.json')
-question = JSON.parse(file)
-subject = {}
+  file = File.read('Physics.json')
+  question = JSON.parse(file)
+  subject = {}
+  #basedir = '.'
+  #alljson = Dir.glob("*.json")
+  #puts alljson
+  #fin = Time.now + 5 * 60; while Time.now < fin; puts fin  - Time.now; sleep 1; end
 
+    puts "Use the Numerical Keys to navigate around the questions"
+    puts
+    puts "+--+--------------------------+--+
+  |  | CONSOLE QUIZ APPLICATION |  |
+  +--+--------------------------+--+
+  +  +      Made by Enemali     +  +
+  |  |                          |  |
+  +  +         at               +  +
+  |  |                          |  |
+  +  +  Andela Boot Camp        +  +
+  |  |        2016              |  |
+  +--+--------------------------+--+
+  |  |         Andela           |  |
+  +--+--------------------------+--+"
+    puts " Enter 1 to start the quiz and your time will beging "
+    askuser = false
+    while !askuser do
+      request = gets.to_i
+      if request == 1
+        askuser = true
+      end
 
-
-
-#basedir = '.'
-#alljson = Dir.glob("*.json")
-#puts alljson
-
-
-  puts "Use the Numerical Keys to navigate around the questions"
-  puts
-  puts "+--+--------------------------+--+
-|  | CONSOLE QUIZ APPLICATION |  |
-+--+--------------------------+--+
-+  +      Made by Enemali     +  +
-|  |                          |  |
-+  +         at               +  +
-|  |                          |  |
-+  +  Andela Boot Camp        +  +
-|  |        2016              |  |
-+--+--------------------------+--+
-|  |         Andela           |  |
-+--+--------------------------+--+"
-  puts " Enter 1 to start the quiz and your time will beging "
-  askuser = false
-  while !askuser do
-    request = gets.to_i
-    if request == 1
-      askuser = true
     end
-
-  end
-  system ("cls")
-
-
-
-start = false
-  puts " Numerical keys to switch to another question"
-while !start
-  qsn = gets.to_i-1
-  if qsn.between?(0,5)
-    subject = question.values[qsn]
     system ("cls")
-    puts subject.keys[0]
-    puts
-    puts subject.values[0]
-    puts
-    puts subject.keys[1]+" "+subject.values[1]
-    puts subject.keys[2]+" "+subject.values[2]
-    puts subject.keys[3]+" "+subject.values[3]
-    puts subject.keys[4]+" "+subject.values[4]
-    puts
-    puts "Use the keyboard to select A  ,  B  , C  or  D"
-    myans = gets.chomp
-    if "D" == myans.upcase
-      myscore +=1
-      puts myscore
+
+  start = false
+    puts " Numerical keys to switch to another question"
+    puts " The timer will start Immediately "
+
+  while !start
+
+    qsn = gets.to_i-1
+    if qsn.between?(0,5)
+      subject = question.values[qsn]
+      system ("cls")
+      puts subject.keys[0]
+      puts
+      puts subject.values[0]
+      puts
+      puts subject.keys[1]+" "+subject.values[1]
+      puts subject.keys[2]+" "+subject.values[2]
+      puts subject.keys[3]+" "+subject.values[3]
+      puts subject.keys[4]+" "+subject.values[4]
+      puts
+      puts "Use the keyboard to select A  ,  B  , C  or  D"
+      myans = gets.chomp
+      if "D" == myans.upcase
+        myscore +=1
+        puts "correct"
+        puts "Now select the next question with the number keys"
+      else
+        puts "wrong"
+        puts "Now select the next question with the number keys"
+      end
     else
-      puts "no"
+      puts "Enter Numbers between 0 to 5"
+      system ("cls")
+      puts "Enter Numbers between 0 to 5"
     end
-  else
-    puts "Enter Numbers between 0 to 5"
-    system ("cls")
-    puts "Enter Numbers between 0 to 5"
   end
-end
-end
+
 
